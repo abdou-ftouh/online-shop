b163dcdbe9a97ad34bcb47a9870c799645ae175a
abdou-ftouh <elftouh13@gmail.com>
2022-04-19 21:56:03 -0400

    create views pages with bootstrap, using EJS and collect my list products

--- controllers/productControler.js
+++ controllers/productControler.js
@@ -3,12 +3,28 @@ const express=require('express');
 const router= express.Router();
 const Product= require('../models/productModel')
 
-
-router.get('/',(req,res)=>{
+router.get('/product',(req,res)=>{
     Product.find({})
     .then((data)=>{
-        res.send(data)
+        res.render('index',{stuff:data})
+        // res.send(data)
     })
 })
-
+router.get('/product/:id',(req,res)=>{
+    Product.findById(req.params.id)
+    .then((data)=>{
+        res.render('product',{data})
+    })
+})
+router.get('/add',(req,res)=>{
+res.render('addProd')
+})
+router.get('/mngProduct',(req,res)=>{
+    Product.find({})
+    .then((data)=>{
+        res.render('mangeProd',{data})
+        // res.send(data)
+    })
+    })
+    
 module.exports=router;
